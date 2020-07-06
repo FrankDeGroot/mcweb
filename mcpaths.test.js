8ec5a57e5c4eb3e3467a9264808b77adfdf6f129
Frank de Groot - Schouten <fjtdg@hotmail.com>
2020-07-06 20:52:56 +0200

    mcpaths.test.js

---
+++ mcpaths.test.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const {
+	currentVersionPath,
+	currentWorldPath,
+	serverPath,
+	versionPath,
+} = require('./mcpaths')
+
+test('currentVersionPath', () => {
+	expect(currentVersionPath).toBe('../server/current')
+})
+
+test('currentWorldPath', () => {
+	expect(currentWorldPath('someVersion')).toBe('../server/someVersion/world')
+})
+
+test('serverPath', () => {
+	expect(serverPath).toBe('../server')
+})
+
+test('versionPath', () => {
+	expect(versionPath('someVersion')).toBe('../server/someVersion')
+})
