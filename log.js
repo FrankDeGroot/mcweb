824dbc71ea1da7af7fc484381cd0811c38419dff
Frank de Groot - Schouten <fjtdg@hotmail.com>
2020-09-01 08:21:26 +0200

    unit test log

--- log.js
+++ log.js
@@ -4,11 +4,15 @@ const levels = ['trace', 'info', 'warning', 'error']
 var min = 0
 
 exports.log = log
-exports.level = level => min = levels.indexOf(level)
+exports.level = level => min = minLevel(level)
 for(const level of levels) {
 	exports[level] = (...args) => log(level, ...args)
 }
 
 function log(level, ...args) {
-	if (levels.indexOf(level) >= min) console[level](new Date().toISOString(), level, ...args)
+	if (minLevel(level) >= min) console[level](new Date().toISOString(), level, ...args)
+}
+
+function minLevel(level) {
+	return levels.indexOf(level)
 }
---
+++ log.test.js
@@ -0,0 +1,87 @@
+'use strict'
+
+global.console.trace = jest.fn()
+global.console.info = jest.fn()
+global.console.warning = jest.fn()
+global.console.error = jest.fn()
+
+const { level, log, trace, info, warning, error } = require('./log')
+
+describe('trace', () => {
+	beforeEach(() => {
+		global.console.trace.mockReset()
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		trace('test')
+		expect(global.console.trace.mock.calls.length).toBe(1)
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		log('trace', 'test')
+		expect(global.console.trace.mock.calls.length).toBe(1)
+	})
+	it('should not log when level set to info', () => {
+		level('info')
+		trace('test')
+		expect(global.console.trace.mock.calls.length).toBe(0)
+	})
+})
+
+describe('info', () => {
+	beforeEach(() => {
+		global.console.info.mockReset()
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		info('test')
+		expect(global.console.info.mock.calls.length).toBe(1)
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		log('info', 'test')
+		expect(global.console.info.mock.calls.length).toBe(1)
+	})
+	it('should not log when level set to info', () => {
+		level('warning')
+		info('test')
+		expect(global.console.info.mock.calls.length).toBe(0)
+	})
+})
+
+describe('warning', () => {
+	beforeEach(() => {
+		global.console.warning.mockReset()
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		warning('test')
+		expect(global.console.warning.mock.calls.length).toBe(1)
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		log('warning', 'test')
+		expect(global.console.warning.mock.calls.length).toBe(1)
+	})
+	it('should not log when level set to warning', () => {
+		level('error')
+		warning('test')
+		expect(global.console.warning.mock.calls.length).toBe(0)
+	})
+})
+
+describe('error', () => {
+	beforeEach(() => {
+		global.console.error.mockReset()
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		error('test')
+		expect(global.console.error.mock.calls.length).toBe(1)
+	})
+	it('should log when level set to trace', () => {
+		level('trace')
+		log('error', 'test')
+		expect(global.console.error.mock.calls.length).toBe(1)
+	})
+})
