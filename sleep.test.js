5118afc04161290a6141b4ea0b699ecbee46361f
Frank de Groot - Schouten <fjtdg@hotmail.com>
2020-09-11 21:02:57 +0200

    test sleep

---
+++ sleep.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { sleep } = require('./sleep')
+
+describe('sleep', () => {
+	beforeEach(() => {
+		jest.useFakeTimers()
+	})
+	it('should return Promise that resolves after timeout', () => {
+
+		sleep(1000)
+		
+		expect(setTimeout).toHaveBeenCalledTimes(1)
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
+	})
+})
