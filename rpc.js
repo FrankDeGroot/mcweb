1a233c593a17f0cbfc0af567cfcd7a58795ea3d2
Frank de Groot - Schouten <fjtdg@hotmail.com>
2020-09-27 22:05:03 +0200

    + rpc_reply.js

--- rpc.js
+++ rpc.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const { info, log } = require('./log')
-
+const { Replier } = require('./rpc_reply')
 const {
   versions,
   currentVersion,
@@ -15,64 +14,25 @@ const {
   update
 } = require('./update/update')
 
-exports.setup = (socket, server) => {
-  function notify (message) {
-    info(message)
-    server.emit('message', message)
-  }
-
-  async function longReply (doing, done, action) {
-    server.emit(doing)
-    try {
-      await action()
-    } catch (err) {
-      replyError(err)
-    } finally {
-      server.emit(done)
-    }
-  }
-
-  async function reply (name, getReply) {
-    try {
-      socket.emit(name, await getReply())
-    } catch (err) {
-      replyError(err)
-    }
-  }
-
-  function replyError (err) {
-    log(err.code ? 'error' : 'info', err)
-    socket.emit('throw', err.code ? 'An internal error occurred' : err.message)
-  }
-
-  socket.replyOn = (name, replier) =>
-    socket.on(name, (...args) =>
-      reply(name, () => replier(...args)))
-
-  socket.longReplyOn = (name, doing, done, replier) =>
-    socket.on(name, (...args) =>
-      longReply(doing, done, () => replier(...args)))
-
-  socket
-    .replyOn('worlds', async version => ({
+exports.setup = (socket, server) => new Replier(socket, server)
+  .replyOn('worlds', async version => ({
+    worlds: await worlds(version),
+    world: await currentWorld(version)
+  }))
+  .replyOn('current', async () => {
+    const version = await currentVersion()
+    return {
+      versions: await versions(),
+      version: version,
       worlds: await worlds(version),
       world: await currentWorld(version)
-    }))
-    .replyOn('current', async () => {
-      const version = await currentVersion()
-      return {
-        versions: await versions(),
-        version: version,
-        worlds: await worlds(version),
-        world: await currentWorld(version)
-      }
-    })
-    .longReplyOn('change', 'changing', 'changed', changeParameters => {
-      const { version, world } = changeParameters
-      return change(version, world, message => notify(message))
-    })
-    .longReplyOn('update', 'updating', 'updated', updateParameters => {
-      const { version } = updateParameters
-      return update(version, message => notify(message))
-    })
-}
+    }
+  })
+  .longReplyOn('change', 'changing', 'changed', (notify, changeParameters) => {
+    const { version, world } = changeParameters
+    return change(version, world, notify)
+  })
+  .longReplyOn('update', 'updating', 'updated', (notify, updateParameters) => {
+    const { version } = updateParameters
+    return update(version, notify)
+  })
---
+++ rpc_reply.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const { info, log } = require('./log')
+
+exports.Replier = function (socket, server) {
+  function notify (message) {
+    info(message)
+    server.emit('message', message)
+  }
+
+  async function longReply (doing, done, action) {
+    server.emit(doing)
+    try {
+      await action()
+    } catch (err) {
+      replyError(err)
+    } finally {
+      server.emit(done)
+    }
+  }
+
+  async function reply (name, getReply) {
+    try {
+      socket.emit(name, await getReply())
+    } catch (err) {
+      replyError(err)
+    }
+  }
+
+  function replyError (err) {
+    log(err.code ? 'error' : 'info', err)
+    socket.emit('throw', err.code ? 'An internal error occurred' : err.message)
+  }
+
+  this.replyOn = (name, handler) => {
+    socket.on(name, (...args) =>
+      reply(name, () => handler(...args)))
+    return this
+  }
+
+  this.longReplyOn = (name, doing, done, handler) => {
+    socket.on(name, (...args) =>
+      longReply(doing, done, () => handler(notify, ...args)))
+    return this
+  }
+}
