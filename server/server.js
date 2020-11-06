be6412898cd36b5551e77ded6fe74d029c0ce25d
Frank de Groot - Schouten <fjtdg@hotmail.com>
2020-11-06 22:01:13 +0100

    upgrade to socket.io 3 and some other minor upgrades

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const { listen } = require('socket.io')
+const io = require('socket.io')
 const { level } = require('./utils/log')
 const { setup } = require('./rpc/setup')
 
 level('info')
 
-const server = listen(1024)
+const server = io(1024)
 server.on('connection', socket => setup(socket, server))
--- server/server.test.js
+++ server/server.test.js
@@ -4,7 +4,7 @@ jest.mock('socket.io')
 jest.mock('./utils/log')
 jest.mock('./rpc/setup')
 
-const { listen } = require('socket.io')
+const io = require('socket.io')
 const { level } = require('./utils/log')
 const { setup } = require('./rpc/setup')
 
@@ -12,7 +12,7 @@ const server = {
   on: jest.fn()
 }
 
-listen.mockImplementation(() => server)
+io.mockImplementation(() => server)
 
 const serverOnHandlers = {}
 server.on.mockImplementation((name, handler) => {
@@ -25,7 +25,7 @@ test('server', () => {
   require('./server')
 
   expect(level).toHaveBeenCalledWith('info')
-  expect(listen).toHaveBeenCalledWith(1024)
+  expect(io).toHaveBeenCalledWith(1024)
   expect(server.on).toHaveBeenCalledWith('connection', expect.any(Function))
 
   serverOnHandlers.connection(socket)
