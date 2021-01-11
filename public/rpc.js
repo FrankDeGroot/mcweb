d826d0903fbdbe6b12a976cd32f9cee3cf3e5775
Frank de Groot - Schouten <fjtdg@hotmail.com>
2021-01-11 20:14:31 +0100

    split up handlers

--- public/rpc.js
+++ public/rpc.js
@@ -9,27 +9,29 @@ import { OpsViewModel } from './ops/ops_view_model.js'
 
 export function connectedViewModel (changeScheduler) {
   const socket = io()
-  const handlers = {
+  const busyViewModel = new BusyViewModel({
     onReady: () => {
       socket.emit('current')
-    },
-    onUpdateVersion: version => socket.emit('update', { version }),
+    }
+  }, changeScheduler)
+  const changeViewModel = new ChangeViewModel({
     onChangeVersionAndWorld: (version, world) => socket.emit('change', {
       version,
       world
-    }),
+    })
+  }, changeScheduler)
+  const createViewModel = new CreateViewModel({
     onCreateWorld: (version, world, seed) => socket.emit('create', {
       version,
       world,
       seed
     })
-  }
-  const busyViewModel = new BusyViewModel(handlers, changeScheduler)
-  const changeViewModel = new ChangeViewModel(handlers, changeScheduler)
-  const createViewModel = new CreateViewModel(handlers, changeScheduler)
-  const updateViewModel = new UpdateViewModel(handlers)
+  }, changeScheduler)
+  const updateViewModel = new UpdateViewModel({
+    onUpdateVersion: version => socket.emit('update', { version })
+  })
   const messagesViewModel = new MessagesViewModel(changeScheduler)
-  const opsViewModel = new OpsViewModel(handlers, changeScheduler)
+  const opsViewModel = new OpsViewModel({}, changeScheduler)
   socket
     .on('message', message => messagesViewModel.pushMessage(message))
     .on('throw', message => messagesViewModel.pushError(message))
@@ -37,6 +39,12 @@ export function connectedViewModel (changeScheduler) {
       busyViewModel.busy = true
     })
     .on('changed', () => {
+      busyViewModel.busy = false
+    })
+    .on('creating', () => {
+      busyViewModel.busy = true
+    })
+    .on('created', () => {
       busyViewModel.busy = false
     })
     .on('updating', () => {
