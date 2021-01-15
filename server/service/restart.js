6852abd65a393e1ddf2da6d96aadd5216c3f39e9
Frank de Groot - Schouten <fjtdg@hotmail.com>
2021-01-15 16:15:08 +0100

    #24 remove restart semaphore

--- server/service/restart.js
+++ server/service/restart.js
@@ -4,27 +4,17 @@ const { start, stop } = require('./service')
 const { say } = require('./rcon')
 const { sleep } = require('../utils/sleep')
 
-let restarting = false
-
 exports.restart = async (reason, notify, reconfigure) => {
-  if (restarting) {
-    throw new Error('Already restarting')
-  }
-  try {
-    restarting = true
-    notify('Send warning to players and wait')
-    await say(`${reason} in 2 seconds.`)
-    await sleep(2000)
-    notify('Stopping Minecraft')
-    await stop()
-    notify('Reconfiguring')
-    reconfigure()
-    notify('Starting Minecraft')
-    await start()
-    notify('Waiting for Minecraft')
-    await say('Welcome back!')
-    notify('Done')
-  } finally {
-    restarting = false
-  }
+  notify('Send warning to players and wait')
+  await say(`${reason} in 2 seconds.`)
+  await sleep(2000)
+  notify('Stopping Minecraft')
+  await stop()
+  notify('Reconfiguring')
+  reconfigure()
+  notify('Starting Minecraft')
+  await start()
+  notify('Waiting for Minecraft')
+  await say('Welcome back!')
+  notify('Done')
 }
--- server/service/restart.test.js
+++ server/service/restart.test.js
@@ -30,24 +30,4 @@ describe('restart', () => {
     expect(reconfigure.mock.calls.length).toBe(1)
     expect(start.mock.calls.length).toBe(1)
   })
-  it('should skip when already restarting', async () => {
-    say.mockImplementationOnce(async () => {
-      await expect(restart('some reason 2', notify, reconfigure)).rejects.toStrictEqual(new Error('Already restarting'))
-    })
-
-    await restart('some reason 1', notify, reconfigure)
-
-    expect(stop.mock.calls.length).toBe(1)
-    expect(reconfigure.mock.calls.length).toBe(1)
-    expect(start.mock.calls.length).toBe(1)
-  })
-  it('should not skip when changing fails', async () => {
-    const ERR = new Error()
-    notify.mockImplementationOnce(() => {
-      throw ERR
-    })
-
-    await expect(restart('some reason 1', notify, reconfigure)).rejects.toBe(ERR)
-    await restart('some reason 2', notify, reconfigure)
-  })
 })
