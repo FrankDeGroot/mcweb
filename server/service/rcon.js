bc4bfac6eff0097abb21e1ff2b7bbe28aa76dc66
Frank de Groot - Schouten <fjtdg@hotmail.com>
2021-01-25 07:00:39 +0100

    #28 remove server properties cache

--- server/service/rcon.js
+++ server/service/rcon.js
@@ -4,22 +4,16 @@ const Rcon = require('rcon')
 const { info } = require('../utils/log')
 const { readServerProperties } = require('../worlds/serverproperties')
 
-let serverProperties = null
-
-async function cacheServerProperties () {
-  if (!serverProperties) {
-    serverProperties = await readServerProperties()
-  }
-}
-
 exports.send = async message => {
-  await cacheServerProperties()
+  const serverProperties = await readServerProperties()
   return new Promise((resolve, reject) => {
     let response = null
-    const con = new Rcon('localhost', serverProperties['rcon.port'], serverProperties['rcon.password'], {
-      tcp: true,
-      challenge: false
-    })
+    const con = new Rcon('localhost',
+      serverProperties['rcon.port'],
+      serverProperties['rcon.password'], {
+        tcp: true,
+        challenge: false
+      })
       .on('auth', () => con.send(message))
       .on('end', () => resolve(response))
       .on('error', err => reject(err))
