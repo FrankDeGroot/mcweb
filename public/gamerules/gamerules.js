f7a4c09f9c3cf9850c3c20bd8ae0040e9b060c4a
Frank de Groot - Schouten <fjtdg@hotmail.com>
2021-01-30 22:16:16 +0100

    #28 handle unknown gamerule type

--- public/gamerules/gamerules.js
+++ public/gamerules/gamerules.js
@@ -22,6 +22,7 @@ export function Gamerules () {
                 value
               })
             ])
+            default: return m('div', `Unknown gamerule type '${type}'.`)
           }
         })
   }
