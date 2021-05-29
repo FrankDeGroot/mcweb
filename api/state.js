afb304ad1c0fb8ef4ef6a6eb35365dd5fcc510d9
FrankDeGroot <fjtdg@hotmail.com>
2021-05-29 08:33:40 +0000

    state private

--- api/state.js
+++ api/state.js
@@ -3,13 +3,13 @@ import { EventEmitter } from 'events'
 export const state = new class extends EventEmitter {
 	#state = {}
 	reset() {
-		this.state = {}
+		this.#state = {}
 	}
 	get current() {
-		return this.state
+		return this.#state
 	}
 	update(delta) {
-		this.state = { ...this.state, ...delta }
+		this.#state = { ...this.#state, ...delta }
 		this.emit('update', delta)
 	}
 }
